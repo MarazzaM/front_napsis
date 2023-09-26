586f2a6a3227f04546d27b206977914f2ca408e9
MarazzaM <marazza.matias@gmail.com>
2023-09-25 21:18:19 -0300

    added questions

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,16 @@ export default function Form() {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [message, setMessage] = useState('');
+  const [empresa, setEmpresa] = useState('');
+  const [empleados, setEmpleados] = useState('');
+  const [industria, setIndustria] = useState('');
+  const [napsis, setNapsis] = useState('');
+  const [sueldo, setSueldo] = useState('');
+  const [digital, setDigital] = useState('');
+  const [integrado, setIntegrado] = useState('');
+  const [conocer, setConocer] = useState('');
+  const [informacion, setInformacion] = useState('');
+
   const router = useRouter()
 
 
@@ -43,7 +53,7 @@ export default function Form() {
   };
 
   return (
-    <div className='grid justify-center items-center h-screen'>
+    <div className='grid justify-center items-center min-h-screen'>
       {/* <h1>Save Person Data</h1> */}
       <section className="bg-white p-16 rounded flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0 ">
       <div className="md:w-1/3 max-w-sm">
@@ -57,23 +67,122 @@ export default function Form() {
       <div className="md:w-1/3 max-w-sm">
         
         <div className="my-5 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-          <p className="mx-4 mb-0 text-center font-semibold text-slate-800">Ingresa a participar</p>
+          <p className="mx-4 mb-0 text-center font-semibold text-slate-800">Sorteo Napsis</p>
         </div>
         <form onSubmit={handleSubmit}>
 
-        <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded " type="text" placeholder="Nombre" value={name}
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="Empresa">
+        Empresa
+      </label>
+        <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mb-4" type="text" placeholder="Empresa" value={empresa}
+            onChange={(e) => setEmpresa(e.target.value)}
+            required />
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="Nombre">
+        Nombre y Apellido
+      </label>
+        <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mb-4" type="text" placeholder="Nombre" value={name}
             onChange={(e) => setName(e.target.value)}
             required />
-
-        <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="email" placeholder="Email" 
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="Email">
+        Email
+      </label>
+        <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mb-4" type="email" placeholder="Email" 
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required/>
-        <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="phone" placeholder="Celular" value={phone} minLength="8" maxLength="20"
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="Celular">
+                    Celular
+      </label>
+        <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mb-4" type="phone" placeholder="Celular" value={phone} minLength="8" maxLength="20"
             onChange={(e) => setPhone(e.target.value)}
             required />
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="Celular">
+          Cantidad de empleados
+      </label>
+<input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mb-4" type="text" placeholder="empleados" 
+            value={empleados}
+            onChange={(e) => setEmpleados(e.target.value)}
+            required/>
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="industria">
+          A que industria pertenece
+      </label>
+<input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mb-4" type="text" placeholder="industria" 
+            value={industria}
+            onChange={(e) => setIndustria(e.target.value)}
+            required/>
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="napsis">
+          ¿Conocen Napsis?
+      </label>
+<select
+    className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mb-4"
+    value={napsis}
+    onChange={(e) => setNapsis(e.target.value)}
+    required
+  >
+    <option value="Si">Si</option>
+    <option value="No">No</option>
+    {/* Add more options as needed */}
+  </select>
+  <label class="block text-gray-700 text-sm font-bold mb-2" for="sueldo">
+          ¿En tu empresa tienen recibo de sueldo digital? 
+      </label>
+  <select
+    className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mb-4"
+    value={sueldo}
+    onChange={(e) => setSueldo(e.target.value)}
+    required
+  >
+    <option value="Si">Si</option>
+    <option value="No">No</option>
+    {/* Add more options as needed */}
+  </select>
+  <label class="block text-gray-700 text-sm font-bold mb-2" for="digital">
+  ¿Cual de estas plataformas digitales tienen en tu empresa?
+</label>
+  <select
+    className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mb-4"
+    value={digital}
+    onChange={(e) => setDigital(e.target.value)}
+    required
+  >
+    <option value="Elearning">Elearning</option>
+    <option value="Legajo Digital">Legajo Digital</option>
+    <option value="Encuesta de Clima">Encuesta de Clima</option>
+    <option value="Evaluación de Desempeño">Evaluación de Desempeño</option>
+    <option value="otro">Otro</option>
+    {/* Add more options as needed */}
+  </select>
+  <label class="block text-gray-700 text-sm font-bold mb-2" for="integrado">
+  ¿Es un sistema integrado o son distintos?
+</label>
+<input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mb-4" type="text" placeholder="integrado" 
+            value={integrado}
+            onChange={(e) => setIntegrado(e.target.value)}
+            required/>  
+
+<label class="block text-gray-700 text-sm font-bold mb-2" for="conocer">
+¿Tienen interés de conocer el legajo digital?
+</label>
+<input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mb-4" type="text" placeholder="conocer" 
+            value={conocer}
+            onChange={(e) => setConocer(e.target.value)}
+            required/>  
+<label class="block text-gray-700 text-sm font-bold mb-2" for="informacion">
+¿Quieren recibir información de Napsis?
+</label>
+<select
+    className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mb-4"
+    value={informacion}
+    onChange={(e) => setInformacion(e.target.value)}
+    required
+  >
+    <option value="Si">Si</option>
+    <option value="No">No</option>
+  </select>
+
+
         <div className="text-center md:text-left">
-          <button className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider" type="submit">Entrar</button>
+          <button className="mb-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider" type="submit">Entrar</button>
         </div>
         </form>
 
